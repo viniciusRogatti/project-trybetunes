0e0234f6f342db61e3f047e334398cb97aff9664
vinicius <vinnyzsouza@gmail.com>
2022-12-12 10:15:14 -0300

    add scripts deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom'
+
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
